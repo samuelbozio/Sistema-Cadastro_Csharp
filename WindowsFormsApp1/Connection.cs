273c78208440a8e12c8f23f9119ce9a7a1f83593
Samuel Bozio <samuelbozio@hotmail.com>
2023-08-29 22:21:32 -0300

    realizando conexao com banco de dados mysql

---
+++ WindowsFormsApp1/Connection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    internal class Connection
+    {
+        public string connect = "SERVER-localhost; DATABASE-banco_de_dados; UID-root, PWD-; PORT-3307;";
+
+        public MySqlConnection connection = null;
+    }
+}
